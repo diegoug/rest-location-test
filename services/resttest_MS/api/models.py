9cc5ad60f2240b8548e133bc1094d8a3f2f2aa3d
DiegoUG <diego.uribe.gamez@gmail.com>
2021-06-27 22:28:37 -0500

    [add] api models

--- services/resttest_MS/api/models.py
+++ services/resttest_MS/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Driver(models.Model):
+    name = models.CharField(
+        'name', max_length=30)
+    services = models.ManyToManyField('api.Service')
+
+class Service(models.Model):
+    workshop = models.ManyToManyField(
+        'api.Workshop')
+    date = models.DateTimeField(
+        'date', default=timezone.now)
+    latitude = models.DecimalField(
+        max_digits=15, decimal_places=11)
+    longitude = models.DecimalField(
+        max_digits=15, decimal_places=11)
+
+class Workshop(models.Model):
+    workshop_id = models.CharField(max_length=30)
+    name = models.CharField(
+        'name', max_length=30)
+    latitude = models.DecimalField(
+        max_digits=15, decimal_places=11)
+    longitude = models.DecimalField(
+        max_digits=15, decimal_places=11)
+    address = models.CharField(max_length=100)
