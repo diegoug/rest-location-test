4445c0aecef517ec8cf89320d1c50eb8a3f90296
DiegoUG <diego.uribe.gamez@gmail.com>
2021-06-27 22:31:58 -0500

    [add] register models in admin

--- services/resttest_MS/api/admin.py
+++ services/resttest_MS/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Driver, Service, Workshop
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    pass
--- services/resttest_MS/api/views.py
+++ services/resttest_MS/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Driver, Service, Workshop
+
 
 class ServiceView(APIView):
     def get(self, request, date):
